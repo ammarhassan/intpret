3ca85b8e20b2378b72670f552cdfa413eefe79d3
ammarhassan <mammar0009@gmail.com>
2016-07-07 01:35:41 -0400

    refactoring custom folder and graphics code placement

--- int_graph/static/custom/piechart.js
+++ int_graph/static/custom/piechart.js
@@ -1,11 +1,10 @@
-function pieChart(data_input, element, title) {
+function pieChart(data_input, id, title) {
     var data = google.visualization.arrayToDataTable(data_input);
 
     var options = {
         title: title,
     };
 
-    var chart = new google.visualization.PieChart(element);
+    var chart = new google.visualization.PieChart(document.getElementById(id));
     chart.draw(data, options);
 }
-
